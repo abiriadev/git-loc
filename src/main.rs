fa596975e17b67af3b3ef26dc4408665b709ace9
Abiria <abiria.dev@gmail.com>
2024-03-08 14:30:30 +0900

    feat: implement basic backbone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use git2::{DiffOptions, Repository, Tree};
+
 fn main() {
-	println!("Hello, world!");
+	let repo = Repository::open(".").unwrap();
+
+	let mut revwalk = repo.revwalk().unwrap();
+
+	revwalk.push_head().unwrap();
+
+	let mut last: Option<Tree> = None;
+
+	for c in revwalk {
+		let c = c.unwrap();
+		let c = repo.find_commit(c).unwrap();
+		let t = c.tree().unwrap();
+
+		let diff = repo
+			.diff_tree_to_tree(
+				last.as_ref(),
+				Some(&t),
+				Some(&mut DiffOptions::new()),
+			)
+			.unwrap();
+
+		let s = diff.stats();
+
+		println!("{:?}", s);
+
+		last = Some(t);
+	}
 }
