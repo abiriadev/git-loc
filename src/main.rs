ca42a3f2d8780164e2b619b3955c5c1dbadd0a92
Abiria <abiria.dev@gmail.com>
2024-04-02 14:36:23 +0900

    feat: use clap parser, add pathspec

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
+use std::path::PathBuf;
+
+use clap::{ArgAction, Parser};
 use git2::{DiffOptions, Repository, Sort, Tree};
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Options {
+	/// Sets the path to the repository
+	#[arg(default_value_t = String::from("."))]
+	repository: String,
+
+	/// Filenames to ignore from statistics
+	#[arg(short, long, action = ArgAction::Append, group = "ignore")]
+	ignore: Vec<String>,
+
+	/// Path to a file that lists filenames to ignore
+	#[arg(short = 'I', long, group = "ignore")]
+	ignore_file: Option<PathBuf>,
+}
+
 fn main() -> anyhow::Result<()> {
-	let repo = Repository::open(".")?;
+	let options = Options::parse();
+
+	let mut diff_option = options.ignore.into_iter().fold(
+		DiffOptions::new(),
+		|mut diff_option, pathspec| {
+			diff_option.pathspec(pathspec);
+			diff_option
+		},
+	);
+
+	let repo = Repository::open(options.repository)?;
 
 	let mut revwalk = repo.revwalk()?;
 
@@ -19,7 +48,7 @@ fn main() -> anyhow::Result<()> {
 			.diff_tree_to_tree(
 				last.as_ref(),
 				Some(&t),
-				Some(&mut DiffOptions::new()),
+				Some(&mut diff_option),
 			)?
 			.stats()?;
 
